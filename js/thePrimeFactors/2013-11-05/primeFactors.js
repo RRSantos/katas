432596cd1633f49e407301d26df2fe823fda6bc6
Rodrigo Ramos <rodrigo.ramos.santos@gmail.com>
2013-11-12 07:26:32 -0200

    Fixed tests and algorithm

--- js/thePrimeFactors/2013-11-05/primeFactors.js
+++ js/thePrimeFactors/2013-11-05/primeFactors.js
@@ -2,7 +2,7 @@ var primeFactors = function(value){
 
 	var factors = [];
 	var divisor = 2;
-	while ((value > 1) && (divisor < value/2)){
+	while ((value > 1) ){
 		if (value % divisor === 0){
 			factors.push(divisor);
 			value = value / divisor;
--- js/thePrimeFactors/2013-11-05/tests/primeFactors.tests.js
+++ js/thePrimeFactors/2013-11-05/tests/primeFactors.tests.js
@@ -2,70 +2,70 @@ var assert = require('assert');
 var primeFactor = require('../primeFactors.js');
 var testFactorsOfTwo = function(){
 	it('testMultiplesOfTwo', function(){
-		assert([2], primeFactor.getPrimeFactorsOf(2));
+		assert.deepEqual([2], primeFactor.getPrimeFactorsOf(2));
 	});
 };
 
 var testFactorsOfThree = function(){
 	it('testMultiplesOfThree', function(){
-		assert([3], primeFactor.getPrimeFactorsOf(3));
+		assert.deepEqual([3], primeFactor.getPrimeFactorsOf(3));
 	});
 };
 
 var testFactorsOfFour = function(){
 	it('testMultiplesOfFour', function(){
-		assert([2,2], primeFactor.getPrimeFactorsOf(4));
+		assert.deepEqual([2,2], primeFactor.getPrimeFactorsOf(4));
 	});
 };
 
 var testFactorsOfFive = function(){
 	it('testMultiplesOfFive', function(){
-		assert([5], primeFactor.getPrimeFactorsOf(5));
+		assert.deepEqual([5], primeFactor.getPrimeFactorsOf(5));
 	});
 };
 
 
 var testFactorsOfSix = function(){
 	it('testMultiplesOfSix', function(){
-		assert([2,3], primeFactor.getPrimeFactorsOf(6));
+		assert.deepEqual([2,3], primeFactor.getPrimeFactorsOf(6));
 	});
 };
 
 var testFactorsOfSeven = function(){
 	it('testMultiplesOfSeven', function(){
-		assert([7], primeFactor.getPrimeFactorsOf(7));
+		assert.deepEqual([7], primeFactor.getPrimeFactorsOf(7));
 	});
 };
 
 var testFactorsOfEight = function(){
 	it('testMultiplesOfEight', function(){
-		assert([2,2,2], primeFactor.getPrimeFactorsOf(8));
+		assert.deepEqual([2,2,2], primeFactor.getPrimeFactorsOf(8));
 	});
 };
 
 
 var testFactorsOfNine = function(){
 	it('testMultiplesOfNine', function(){
-		assert([3,3], primeFactor.getPrimeFactorsOf(9));
+		assert.deepEqual([3,3], primeFactor.getPrimeFactorsOf(9));
 	});
 };
 
 var testFactorsOfTen = function(){
 	it('testMultiplesOfTen', function(){
-		assert([2,5], primeFactor.getPrimeFactorsOf(10));
+		assert.deepEqual([2,5], primeFactor.getPrimeFactorsOf(10));
 	});
 };
 
 var testFactorsOfZero = function(){
 	it('testMultiplesOfZero', function(){
-		assert([], primeFactor.getPrimeFactorsOf(0));
+		assert.deepEqual([], primeFactor.getPrimeFactorsOf(0));
 	});
 };
 
 
 var testFactorsOfSixtyFour = function(){
 	it('testMultiplesOfSixtyFour', function(){
-		assert([2,2,2,2,2,2,2,2], primeFactor.getPrimeFactorsOf(64));
+		assert.deepEqual([2,2,2,2,2,2], primeFactor.getPrimeFactorsOf(64));
 	});
 };
 
